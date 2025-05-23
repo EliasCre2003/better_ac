e6f0a7d36a7f48f02e18382481dd06ee26ede337
Elias Cregård <eliascregard701@gmail.com>
2025-05-23 15:15:59 +0200

    added app element

--- elements.py
+++ elements.py
@@ -4,5 +4,30 @@ import acsys
 
 class _GenericElement:
     def __init__(self, title: str):
-        self.form = ...
+        self.object_id = ...
 
+    # def get_id(self) -> int:
+    #     """
+    #     Get the ID of the element.
+    #     """
+    #     return self.object_id
+    
+    def set_size(self, width: float, height: float) -> None:
+        """
+        Set the size of the element.
+        """
+        ac.setSize(self.object_id, width, height)
+
+    
+
+
+class App(_GenericElement):
+    def __init__(self, title: str = "App"):
+        self.object_id = ac.newApp(title)
+
+    
+    def set_title(self, title: str) -> None:
+        """
+        Set the title of the app.
+        """
+        ac.setTitle(self.object_id, title)
