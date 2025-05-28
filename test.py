4b8333b8e2d016245dd345e459f1b8d2ad646227
Elias Cregård <eliascregard701@gmail.com>
2025-05-28 13:26:47 +0200

    as

--- test.py
+++ test.py
@@ -1,16 +1,21 @@
-class Sak:
-    def __init__(self):
-        self.sak = "sak"
+with open("documentation/checklist.txt", "r") as file:
+    checklist = file.readlines()
 
+script = "better_ac.py"
+with open(script, "r") as file:
+    car_code = file.readlines()
 
-    @property
-    def sak(self):
-        return self._sak
-    
-    @sak.setter
-    def sak(self, value):
-        self._sak = value
+# print("ac.getCarState" in car_code[450])
+# print(any("ac.getCarState" in line for line in car_code))
 
-sak = Sak()
+for func in checklist:
+    pre_format = func
+    func = func.strip()
+    if func and not any(func in line for line in car_code):
+        print(f"Function '{func}' is missing from {script}")
+    else:
+        checklist.remove(pre_format)
 
-print(sak.sak)  # Output: sak
+with open("documentation/checklist.txt", "w") as file:
+    for func in checklist:
+        file.write(func)
